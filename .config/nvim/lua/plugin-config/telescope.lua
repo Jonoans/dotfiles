249851efb5fb724fffac8a3315ced99d79da7328
Jonoans <jonjontan610@hotmail.com>
2023-01-06 20:22:49 +0800

    Disable nvim-tree

--- .config/nvim/lua/plugin-config/telescope.lua
+++ .config/nvim/lua/plugin-config/telescope.lua
@@ -2,12 +2,12 @@ local status, telescope = pcall(require, 'telescope')
 if not status then
     return
 end
-telescope.setup({
+telescope.setup {
     extensions = {
         file_browser = {
-            hijack_netrw = true
-        }
-    }
-})
+            hijack_netrw = true,
+        },
+    },
+}
 telescope.load_extension 'fzf'
 telescope.load_extension 'file_browser'
