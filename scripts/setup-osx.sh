d901d24347c9478e2715eea04b6065b6ff2a5942
Jonoans <jonjontan610@hotmail.com>
2023-01-07 17:53:38 +0800

    Add prettierd and stylua

--- scripts/setup-osx.sh
+++ scripts/setup-osx.sh
@@ -2,8 +2,10 @@
 
 # brew stuff - TODO: add stuff soon
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install neovim tmux
+brew install neovim tmux fsouza/prettierd/prettierd rust
 brew install --cask rectangle
+
+cargo install stylua
 
 # install tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
