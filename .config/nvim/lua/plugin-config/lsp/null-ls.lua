f82a6f5eb1dda70837f518c8459b51ded3a91975
Jonoans <jonjontan610@hotmail.com>
2023-01-30 21:57:11 +0800

    Disable format on save

--- .config/nvim/lua/plugin-config/lsp/null-ls.lua
+++ .config/nvim/lua/plugin-config/lsp/null-ls.lua
@@ -25,21 +25,21 @@ null_ls.setup {
         formatting.stylua,
         diagnostics.eslint_d,
     },
-    on_attach = function(client, bufnr)
-        if client.supports_method 'textDocument/formatting' then
-            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format {
-                        bufnr = bufnr,
-                        filter = function(clt)
-                            return clt.name == 'null-ls'
-                        end,
-                    }
-                end,
-            })
-        end
-    end,
+    -- on_attach = function(client, bufnr)
+    --     if client.supports_method 'textDocument/formatting' then
+    --         vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    --         vim.api.nvim_create_autocmd('BufWritePre', {
+    --             group = augroup,
+    --             buffer = bufnr,
+    --             callback = function()
+    --                 vim.lsp.buf.format {
+    --                     bufnr = bufnr,
+    --                     filter = function(clt)
+    --                         return clt.name == 'null-ls'
+    --                     end,
+    --                 }
+    --             end,
+    --         })
+    --     end
+    -- end,
 }
